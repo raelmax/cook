218e74f50b605ed77dd43609dea9547fb592154b
Rael Max <contato@raelmax.com>
2015-06-20 12:15:00 -0300

    main go file

---
+++ main.go
@@ -0,0 +1,12 @@
+// Cook generate projects from templates based on git repositories.
+package main
+
+import "fmt"
+
+func init() {
+	fmt.Println("Initialized")
+}
+
+func main() {
+	fmt.Println("Hello, world.")
+}
