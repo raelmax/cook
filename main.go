f2ec58320c15425e10a12829f5cda170a4769203
Rael Max <contato@raelmax.com>
2015-06-20 13:35:57 -0300

    added basic method to fetch github repository

--- main.go
+++ main.go
@@ -1,12 +1,44 @@
-// Cook generate projects from templates based on git repositories.
+// Cook generate projects from templates based on github repositories.
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
 
-func init() {
-	fmt.Println("Initialized")
+const (
+	GITHUB_URL = "git@github.com:"
+)
+
+// Clone fetch a git repository to current directory and returns
+// a directory name
+func Clone(repoName string) string {
+	repoUrl := GITHUB_URL + repoName + ".git"
+
+	cloneCmd := exec.Command("git", "clone", repoUrl)
+	cloneOut, err := cloneCmd.Output()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(cloneOut))
+
+	return strings.Split(repoName, "/")[1]
 }
 
 func main() {
-	fmt.Println("Hello, world.")
+	var repoName, repoPath string
+
+	if len(os.Args) > 1 {
+		repoName = os.Args[1]
+	} else {
+		fmt.Println("You must provide a github <username>/<repository>")
+		return
+	}
+
+	repoPath = Clone(repoName)
+	fmt.Println(repoPath)
 }
